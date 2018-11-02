6f7d54e9847e9fcea3159f3765c0d43777d4a334
gekoramy <luka.mosetti@gmail.com>
2018-11-02 15:50:21 +0100

    Fastest solution

--- range.hpp
+++ range.hpp
@@ -2,11 +2,10 @@
 #define RANGE_RANGE_HPP
 
 #include <algorithm>
-#include <set>
 #include <numeric>
-#include <utility>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 class range {
 private:
@@ -14,7 +13,7 @@ private:
     int tl;
 
 public:
-    range(const int hd, const int tl) : hd(hd), tl(tl) {}
+    range(const int hd = 0, const int tl = 0) : hd(hd), tl(tl) {}
 
     const int head() const {
         return hd;
@@ -28,206 +27,78 @@ public:
         return tl - hd;
     };
 
-    const bool empty() const {
-        return length() == 0;
-    }
-
     const bool contains(const int x) const {
         return hd <= x && tl >= x;
     }
 
     bool operator==(const range &rhs) const {
-        return hd == rhs.hd &&
-               tl == rhs.tl;
+        return hd == rhs.hd && tl == rhs.tl;
     }
 
     bool operator!=(const range &rhs) const {
         return !(rhs == *this);
     }
 
-    bool operator<(const range &rhs) const {
-        return this->length() == rhs.length() ? hd > rhs.hd : this->length() < rhs.length();
-    }
-
-    bool operator>(const range &rhs) const {
-        return rhs < *this;
-    }
-
-    bool operator<=(const range &rhs) const {
-        return !(rhs < *this);
-    }
-
-    bool operator>=(const range &rhs) const {
-        return !(*this < rhs);
-    }
-
     friend std::ostream &operator<<(std::ostream &os, const range &range1) {
-        os << "[ " << range1.hd << ", " << range1.tl << "] ";
+        os << "[ " << range1.hd << ", " << range1.tl << " ]";
         return os;
     }
 };
+
+bool head_first(const range &a, const range &b) {
+    return a.head() < b.head();
+}
+
+bool length_first(const range &a, const range &b) {
+    return a.length() == b.length() ? !head_first(a, b) : a.length() < b.length();
+}
 
 class coverage {
 private:
     range r;
-    std::set<range> set;
+    std::vector<range> set;
 
 public:
     explicit coverage(const range &cover) : r(cover), set({}) {}
 
-    coverage(const range &cover, std::set<range> uncover) : r(cover), set(std::move(uncover)) {}
+    void add(const range &x) {
+        if (!r.contains(x.head())) {
+            set.emplace_back(r.tail(), x.head());
+        }
 
-    const range &cover() const {
-        return r;
-    }
-
-    const std::set<range> &uncover() const {
-        return set;
+        r = range(std::min(r.head(), x.head()),
+                  std::max(r.tail(), x.tail()));
     }
 
     const range max() const {
-        return set.empty() || (*set.rbegin()).length() <= 1 ? range(0, 0) : *set.rbegin();
-    }
-
-    friend std::ostream &operator<<(std::ostream &os, const coverage &coverage1) {
-        os << "r: " << coverage1.r << " set: ";
-        for (auto el : coverage1.set) {
-            os << el << " ";
-        }
-        return os;
+        auto max = std::max_element(set.begin(), set.end(), length_first);
+        return max == set.end() || (*max).length() <= 1 ? range(0, 0) : *max;
     }
 };
 
-std::set<range> range_difference(const range &x, const range &y) {
-    bool a = x.contains(y.head());
-    bool b = x.contains(y.tail());
-
-    if (x.empty() || x == y) {
-        return {};
-    }
-
-    if (y.empty()) {
-        return {{x.head(), x.tail()}};
-    }
-
-    // xh yh yt xt
-    if (a && b) {
-        return {{x.head(), y.head()},
-                {y.tail(), x.tail()}};
-    }
-
-    // xh yh xt yt
-    if (a) {
-        return {{x.head(), y.head()}};
-    }
-
-    // yh xh yt xt
-    if (b) {
-        return {{y.tail(), x.tail()}};
-    }
-
-    // yh xh xt yt
-    if (y.contains(x.head()) || y.contains(x.tail())) {
-        return {};
-    }
-
-    // yh yt xh xt
-    return {{x.head(), x.tail()}};
+coverage coverage_add(coverage &c, const range &r) {
+    c.add(r);
+    return c;
 }
 
-std::set<range> range_difference_not_empty(const range &x, const range &y) {
-    std::set<range> tmp = range_difference(x, y);
-    std::set<range> result;
-    for (auto r : tmp) { if (!r.empty()) result.insert(r); }
-    return result;
-}
-
-coverage range_union(const range &x, const range &y) {
-    const range merge(
-            std::min(x.head(), y.head()),
-            std::max(x.tail(), y.tail())
-    );
-
-    if (y.empty()) {
-        return {merge, range_difference_not_empty(merge, x)};
-    }
-
-    if (x.empty()) {
-        return {merge, range_difference_not_empty(merge, y)};
-    }
-
-    std::set<range> diff = range_difference_not_empty(merge, x);
-    std::set<range> result;
-
-    for (auto el : diff) {
-        std::cout << el << " ";
-        std::set<range> ds = range_difference_not_empty(el, y);
-        result.insert(ds.begin(), ds.end());
-    }
-
-    for (auto el : result) {
-        std::cout << el << " ";
-    }
-    std::cout << std::endl;
-
-    return {merge, result};
-}
-
-coverage coverage_union(const coverage &c1, const coverage &c2) {
-    const range merge(
-            std::min(c1.cover().head(), c2.cover().head()),
-            std::max(c1.cover().tail(), c2.cover().tail())
-    );
-
-    std::set<range> result;
-    std::set_union(c1.uncover().begin(), c1.uncover().end(),
-                   c2.uncover().begin(), c2.uncover().end(),
-                   std::inserter(result, result.begin()));
-
-    return {merge, result};
-}
-
-coverage coverage_add(const coverage &c, const range &r) {
-    coverage merge = coverage_union(c, range_union(c.cover(), r));
-
-    std::set<range> result;
-
-    if (c.cover().contains(r.head()) || c.cover().contains(r.tail()) ||
-        (r.contains(c.cover().head()) && r.contains(c.cover().tail()))) {
-        for (auto el : merge.uncover()) {
-            std::set<range> ds = range_difference_not_empty(el, r);
-            result.insert(ds.begin(), ds.end());
-        }
-    } else {
-        result = merge.uncover();
-    }
-
-    coverage f(merge.cover(), result);
-
-    std::cout << c << " + " << r << std::endl << "merge : " << merge << std::endl << f << std::endl << std::endl;
-
-    return f;
-}
-
-coverage fromSet(const std::set<range> &ranges) {
-    return std::accumulate(ranges.begin(), ranges.end(),
-                           coverage(*ranges.begin()),
-                           [](const coverage &c, const range &r) -> coverage { return coverage_add(c, r); });
+coverage fromVector(std::vector<range> &ranges) {
+    std::sort(ranges.begin(), ranges.end(), head_first);
+    return std::accumulate(ranges.begin(), ranges.end(), coverage(*ranges.begin()), coverage_add);
 }
 
 range uncovered(std::istream &input) {
-    int n;
+    unsigned long n;
     int hd, tl;
-    std::set<range> ranges;
 
     input >> n;
 
+    std::vector<range> ranges(n);
     for (auto i = 0; i < n; ++i) {
         input >> hd >> tl;
-        ranges.insert({hd, tl});
+        ranges[i] = range(hd, tl);
     }
 
-    return fromSet(ranges).max();
+    return fromVector(ranges).max();
 }
 
 #endif //RANGE_RANGE_HPP
--- test.cpp
+++ test.cpp
@@ -4,48 +4,51 @@
 #include "range.hpp"
 
 TEST_CASE("test provided : [2, 5], [8, 12], [1, 4], [7, 9] = [5, 7]") {
-    coverage cov = fromSet({{2, 5},
-                            {8, 12},
-                            {1, 4},
-                            {7, 9}});
-    REQUIRE (range(5, 7) == cov.max());
+    std::vector<range> ranges = {{2, 5},
+                                 {8, 12},
+                                 {1, 4},
+                                 {7, 9}};
+    REQUIRE (range(5, 7) == fromVector(ranges).max());
 }
 
 TEST_CASE("test provided : [2, 6], [8, 12], [1, 4], [6, 9] = [0, 0]") {
-    coverage cov = fromSet({{2, 6},
-                            {8, 12},
-                            {1, 4},
-                            {6, 9}});
-    REQUIRE (range{0, 0} == cov.max());
+    std::vector<range> ranges = {{2, 6},
+                                 {8, 12},
+                                 {1, 4},
+                                 {6, 9}};
+    REQUIRE (range{0, 0} == fromVector(ranges).max());
 }
 
 TEST_CASE("[2, 5], [3, 4] = [0, 0]") {
-    coverage cov = fromSet({{2, 5},
-                            {3, 4}});
-    REQUIRE (range{0, 0} == cov.max());
+    std::vector<range> ranges = {{2, 5},
+                                 {3, 4}};
+    REQUIRE (range{0, 0} == fromVector(ranges).max());
 }
 
 TEST_CASE("[2, 5], [20, 24], [8, 9] = [9, 20]") {
-    coverage cov = fromSet({{2,  5},
-                            {20, 24},
-                            {8,  9}});
-    REQUIRE (range{9, 20} == cov.max());
+    std::vector<range> ranges = {{2,  5},
+                                 {20, 24},
+                                 {8,  9}};
+    REQUIRE (range{9, 20} == fromVector(ranges).max());
 }
 
 TEST_CASE("[2, 9], [14, 16], [21, 23] = [9, 14], [16, 21]") {
-    coverage cov = fromSet({{2,  9},
-                            {14, 16},
-                            {21, 23}});
-    REQUIRE (range{9, 14} == cov.max());
+    std::vector<range> ranges = {{2,  9},
+                                 {14, 16},
+                                 {21, 23}};
+    REQUIRE (range{9, 14} == fromVector(ranges).max());
+}
+
+TEST_CASE("[2, 9], [14, 16], [21, 23] = [9, 14], [1, 50]") {
+    std::vector<range> ranges = {{2,  9},
+                                 {14, 16},
+                                 {21, 23},
+                                 {1,  50}};
+    REQUIRE (range{0, 0} == fromVector(ranges).max());
 }
 
 TEST_CASE("[2, 5], [0, 1] = [0, 0]") {
-    coverage cov = fromSet({{2, 5},
-                            {0, 1}});
-    REQUIRE (range{0, 0} == cov.max());
-}
-
-TEST_CASE("[5, 7] - [1, 4] = [5, 7]") {
-    std::set<range> expected = {{5, 7}};
-    REQUIRE (expected == range_difference({5, 7}, {1, 4}));
+    std::vector<range> ranges = {{2, 5},
+                                 {0, 1}};
+    REQUIRE (range{0, 0} == fromVector(ranges).max());
 }
